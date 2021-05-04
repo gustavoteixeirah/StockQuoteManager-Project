3ab8fb3c91718ec30a3ef857adb2eea8f8f3249b
Gustavo Teixeira <iwhrim@gmail.com>
2021-05-04 11:11:40 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Stock Quote Manager Project
---
+++ src/main/java/dev/gustavoteixeira/api/stockquotemanager/StockQuoteManagerApplication.java
@@ -0,0 +1,13 @@
+package dev.gustavoteixeira.api.stockquotemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockQuoteManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockQuoteManagerApplication.class, args);
+	}
+
+}
